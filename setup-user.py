88cd90b77586ab3ed796f81f3199ebe0120655a8
Joshua M Smith <radjosh@gmail.com>
2024-10-27 20:37:19 +0000

    ENV passwords instead

--- setup-user.py
+++ setup-user.py
@@ -10,4 +10,4 @@ USERNAME = os.environ["SUPABASE_USERNAME"]
 PASSWORD = os.environ["SUPABASE_PASSWORD"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-response = supabase.auth.sign_up({"email": "radjosh@gmail.com", "password": "@EFEFefef2"})
+response = supabase.auth.sign_up({"email": USERNAME, "password": PASSWORD})
